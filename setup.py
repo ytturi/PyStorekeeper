69271537431dd63e887ba0a49d3abce089287529
ytturi <ytturi@protonmail.com>
2017-11-19 21:05:20 +0100

    Add setup file for PyStorekeeper

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "PyStorekeeper",
+    version = "0.0.1",
+    author = "Ytturi",
+    author_email = "ytturi@protonmail.com",
+    description = ("A Python Backup tool for Databases and files. Dump,"
+        " compress and unify your data in a single archive."),
+    license = "GNU GENERAL PUBLIC LICENSE v3",
+    keywords = "backup tool click",
+    url = "https://github.com/ytturi/PyStorekeeper",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    install_requires=[
+        'click',
+        'logging',
+    ],
+    entry_points='''
+        [console_scripts]
+        py_archive=PyStorekeeper.py_archive:py_archive
+    '''
+)
