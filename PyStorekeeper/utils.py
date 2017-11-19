1aa9c7da85c6568f490a817793c5cd83450751da
ytturi <ytturi@protonmail.com>
2017-11-19 22:21:57 +0100

    Add docstring to utils methods

--- PyStorekeeper/utils.py
+++ PyStorekeeper/utils.py
@@ -5,6 +5,15 @@ import logging
 
 
 def _log_msg(msg='', log_type='info'):
+     """
+     Private method to log messages using logger
+     :param msg:      Message to log
+     :type msg:       str
+     :param log_type: Log type (from logging) to use
+     :type log_type:  str
+     :return:         TRUE if logged with any of the available log_types
+     :rtype:          bool
+     """
      logger = logging.getLogger()    
      if log_type.lower() == 'info':
          logger.info(msg)
@@ -22,6 +31,17 @@ def _log_msg(msg='', log_type='info'):
    
 
 def verbose_log(msg=False, log_type='info', level=0):
+    """
+    Method to log using logger type and verbose level from confs
+    :param msg:      Message to log
+    :type msg:       str
+    :param log_type: Logging type to use
+    :type log_type:  str
+    :param level:    Verbose level required to log the message
+    :type level:     int
+    :return:         TRUE if message is logged, else FALSE
+    :rtype:          bool
+    """
     logger = logging.getLogger()
     if 'confs' not in globals().keys():
         logger.error('Trying to log without initialize!')
@@ -36,6 +56,18 @@ def verbose_log(msg=False, log_type='info', level=0):
 
 
 def initialize(verbose_level=0, conf_path=None):
+    """
+    Method to initialize the Storekeeper
+    - Init logger with a formatter and level
+    - Init confs with default values
+    - Import confs from configuration file if provided
+    :param verbose_level: Verbose level provided by command_line args
+    :type verbose_level:  int
+    :param conf_path:     Configuration file path to read and import
+    :type conf_path:      str
+    :return:              Configurations using default confs and imported
+    :rype:                dict
+    """
     logging.basicConfig(
         format='%(asctime)s%(levelname)s| %(message)s',
         datefmt='[%Y/%m/%d-%H:%M:%S]',
