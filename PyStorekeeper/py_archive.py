3fd74e61a1aa027007785ff7146f4c6150f81d16
ytturi <ytturi@protonmail.com>
2017-11-19 22:10:07 +0100

    Add fs archiving if found in configs

--- PyStorekeeper/py_archive.py
+++ PyStorekeeper/py_archive.py
@@ -1,4 +1,5 @@
 from PyStorekeeper.utils import *
+from PyStorekeeper import files_archive
 import click
 import os
 
@@ -10,4 +11,6 @@ import os
     default='', type=str, help='Configuration file to import (JSON)')
 def py_archive(verbose, config):
     confs = initialize(verbose_level=verbose, conf_path=config)
-
+    fs_paths = confs.get('paths', [])
+    for fs_path in fs_paths:
+        files_archive.archive_path(fs_path)
